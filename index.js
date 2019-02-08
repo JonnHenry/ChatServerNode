b7dd6857336009ef7163a1ba98f1917d940b9efa
Jonnathan Campoberde <jonnathanca9601@gmail.com>
2019-02-08 12:37:05 -0500

    Chat con Node
    Servidor de chat en Node

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io =require('socket.io')(server);
+
+app.use(express.static('client'));
+
+app.get('/chat',function(req,res){
+    res.status(200).send('hola mundo desde una ruta');
+});
+
+var messages =[{
+    id:1,
+    text: 'Bienvenido al chat',
+    nickname:'FastServices'
+}];
+
+io.on('connection',function(socket){
+    console.log("El cliente con IP: "+ socket.handshake.address+" se ha conectado...");
+
+    socket.emit('messages',messages);
+
+    socket.on('add-message',function(data){
+        messages.push(data);
+
+        io.sockets.emit('messages',messages);
+    });
+});
+
+server.listen(6677,function(){
+    console.log('Servidor está funcionando en http://localhost:6677');
+});
+
